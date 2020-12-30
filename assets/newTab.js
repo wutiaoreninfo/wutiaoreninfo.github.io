9e4f2a0b2879505f22c623e3527b340ebb966605
wutiaoreninfo <wutiaoreninfo@163.com>
2020-12-30 11:22:11 +0800

    add rel to links

--- assets/newTab.js
+++ assets/newTab.js
@@ -5,6 +5,7 @@ for (i=0;i<mylinks.length-1;i++) {
     var mylink = mylinks[i];
     if (!(mylink.href.includes("//localhost:4000/") || mylink.href.includes("//wutiaoren.info/"))) {
         mylink.target="_blank";
+        mylink.rel="noopener noreferrer";
     }
     console.log(mylink)
 }
